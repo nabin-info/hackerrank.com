61290c0ea997b8387cd1875f0e2691f15465ddff
Ryan Nabinger <ryan.nabinger@gmail.com>
2016-11-06 12:42:07 -0800

    n6.py calculates the series only once and passed all test cases

--- project_euler/n6.py
+++ project_euler/n6.py
@@ -8,15 +8,19 @@ def _sum2diff(N=0):
     while N == 0 or N >= n:
         s1 += n
         s2 += n * n
-        yield s1*s1 - s2
+        yield (s1, s2)
         n += 1
 
 T = int(raw_input().strip())
+Ns = []
+Nmax = 0
 for t in range(T):
     N = int(raw_input().strip())
-    dif = [] 
-    s2d = _sum2diff(N)
-    for x in s2d:
-        dif += [x] 
-    #print dif
-    print x
+    Ns += [N]
+    if N > Nmax:
+        Nmax = N
+
+## populate Ssum series one time only !
+Ssum = [x for x in _sum2diff(Nmax)]
+for N in Ns:
+    print Ssum[N][0]**2 - Ssum[N][1]
