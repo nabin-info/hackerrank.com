ca53a9f8f4fe783aa1dbda5c64c12e0613f8f404
Ryan Nabinger <ryan.nabinger@gmail.com>
2016-11-10 01:26:35 -0800

    swap-case is trivial in python

---
+++ swap-case.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import math
+
+def input_arg(tr=str): return tr(raw_input().strip())
+def input_args(tr=str): return map(tr, list(input_arg().split(' ')))
+def input_arglines(n,tr=str): return [input_arg(tr) for x in range(n)]
+
+
+sin = input_arg(str)
+sout = sin.swapcase()
+print sout
